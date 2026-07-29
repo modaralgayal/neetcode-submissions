56ebcb2da327a9886c9350397f814182e7ec6f86
modaralgayal <118994907+modaralgayal@users.noreply.github.com>
2026-07-29 14:15:58 +0300

    Add: trapping-rain-water - submission-23

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-23.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size(), total = 0;
+        vector<int> pref(n,0);
+        vector<int> suf(n,0);
+
+        pref[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            pref[i] = max(height[i], pref[i-1]);
+        }
+
+        suf[n-1] = height[n-1];
+        for (int j = n - 2; j >= 0; j--) {
+            suf[j] = max(height[j], suf[j+1]);
+        }
+
+        for (int t = 0; t < n; t++) {
+            total += min(pref[t],suf[t]) - height[t];
+        }
+
+        return total;
+        
+        }
+};
