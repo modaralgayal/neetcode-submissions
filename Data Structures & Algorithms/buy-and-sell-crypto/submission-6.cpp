dc61bc4fff83b63b6712aa6f859e00ac18870a40
modaralgayal <118994907+modaralgayal@users.noreply.github.com>
2026-08-03 15:37:40 +0300

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size(), maxP = -100;
+        pair<int, int> lo = {100, n}, hi = {-100, 0};
+
+
+        for (int i = 0; i < n; i++) {
+            if (prices[i] < lo.first) lo = {prices[i], i};
+            if (prices[i] > lo.first && i > lo.second) hi = {prices[i], i};
+
+            if (hi.first > lo.first && hi.second > lo.second) {
+                maxP = max(maxP, prices[hi.second]- prices[lo.second]);
+            }
+        }
+        return maxP > 0 ? maxP : 0;
+        
+    }
+};
