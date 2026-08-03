fa50faddbe85245e43f2d12e1a6e6e38e1000e6c
modaralgayal <118994907+modaralgayal@users.noreply.github.com>
2026-08-03 16:22:07 +0300

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.size() == 0) return 0;
+        unordered_set<char> hash;
+        int ans = 0, lo = 0, n = s.size();
+        
+        hash.insert(s[lo]);
+        for (int hi = 1; hi < n; hi++) { 
+            
+            while (hash.find(s[hi]) != hash.end()) { // is found
+                hash.erase(s[lo]);
+                lo++;
+            }
+
+            hash.insert(s[hi]);
+            ans = max(ans, hi - lo);
+        }
+
+        return ans + 1;
+    }
+};
