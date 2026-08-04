e595654449987f32966470f5ccc71cd75e1e5c5a
modaralgayal <118994907+modaralgayal@users.noreply.github.com>
2026-08-04 16:36:17 +0300

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.size() == 0) return 0;
+        unordered_set<char> hash;
+        int ans = 1, lo = 0;
+        
+        hash.insert(s[lo]);
+        for (int hi = 1; hi < s.size(); hi++) { 
+            
+            while (hash.find(s[hi]) != hash.end()) { // is found
+                hash.erase(s[lo]);
+                lo++;
+            }
+
+            hash.insert(s[hi]);
+            ans = max(ans, hi - lo + 1);
+        }
+
+        return ans;
+    }
+};
