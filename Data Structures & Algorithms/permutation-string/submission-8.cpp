7541151f5c2c566194dc8fed73807363d1796c40
modaralgayal <118994907+modaralgayal@users.noreply.github.com>
2026-08-04 16:36:04 +0300

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> s1_freq;
+        unordered_map<char, int> s2_freq;
+        int n = s2.size();
+
+        for (auto c : s1) {
+            s1_freq[c]++;
+        }
+
+        sort(s1.begin(), s1.end());
+        string perm;
+
+        for (int hi = 0; hi < n; hi++) {
+            
+            perm += s2[hi];
+            if (perm.size() > s1.size()) {
+                perm = perm.substr(1);
+            }
+            string comp = perm;
+            sort(comp.begin(), comp.end());
+            if (s1 == comp) return true; 
+        }
+
+        return false;
+    }
+};
